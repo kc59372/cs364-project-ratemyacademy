86f3cc3882aa2a65eaf37f8e74b03951c0ebc795
HannahDavis <C27hannah.davis@afacademy.af.edu>
2026-04-03 11:13:56 -0600

    changes: Will's center scrollbar, post methods rather than get

--- server.js
+++ server.js
@@ -2,7 +2,16 @@ const express = require("express");
 
 const app = express();
 
+// for using post rather than get in searches
+// Documentation: https://www.geeksforgeeks.org/web-tech/express-js-app-post-function/
 app.use(express.static("frontend"));
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/search", (req, res) => {
+   const query = req.body.query;
+
+   res.send(query);
+});
 
 app.listen(3000, () => {
    console.log("Node server running");
