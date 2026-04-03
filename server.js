94f83d6292563a9ecb9dd1dc1070dceb6e18a5a1
HannahDavis <C27hannah.davis@afacademy.af.edu>
2026-04-03 09:07:26 -0600

    fixed server.js - used correct name "frontend" for app.use

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static("frontend"));
 
 app.listen(3000, () => {
    console.log("Node server running on port 3000");
